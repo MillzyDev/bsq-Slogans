e6cfa29159c4dee6d6412e996a8831617ea33ec5
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-08-26 08:10:07 +0100

    Update Config.hpp

--- include/Config/Config.hpp
+++ include/Config/Config.hpp
@@ -9,7 +9,9 @@ DECLARE_CONFIG(ModConfig,
                CONFIG_VALUE(TextSize, float, "Size", 60.0f);
                CONFIG_VALUE(TextPos, UnityEngine::Vector3, "Position", UnityEngine::Vector3(0.0f, 10.0f, 26.0f));
                CONFIG_VALUE(TextRot, UnityEngine::Vector3, "Rotation", UnityEngine::Vector3(0.0f, 0.0f, 0.0f));
+               CONFIG_VALUE(IsBloomed, bool, "Bloom", false);
                CONFIG_VALUE(IsRainbow, bool, "Rainbow Mode", false);
+               CONFIG_VALUE(IsInLevel, bool, "Show In Level", false);
                CONFIG_VALUE(TextColor, UnityEngine::Color, "Color", UnityEngine::Color(1.0f, 1.0f, 1.0f, 1.0f));
 
                        CONFIG_INIT_FUNCTION(
@@ -17,7 +19,9 @@ DECLARE_CONFIG(ModConfig,
                                CONFIG_INIT_VALUE(TextSize);
                                CONFIG_INIT_VALUE(TextPos);
                                CONFIG_INIT_VALUE(TextRot);
+                               CONFIG_INIT_VALUE(IsBloomed);
                                CONFIG_INIT_VALUE(IsRainbow);
+                               CONFIG_INIT_VALUE(IsInLevel);
                                CONFIG_INIT_VALUE(TextColor);
                                )
                )
