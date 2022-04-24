277b03758bfd887e643d5072b5132b8ba88208f9
MillzyDev <millzyyt08@gmail.com>
2022-04-24 11:17:00 +0100

    yeet cast

--- src/UI/ViewControllers/SloganSelectionViewController.cpp
+++ src/UI/ViewControllers/SloganSelectionViewController.cpp
@@ -16,10 +16,16 @@ using namespace QuestUI::BeatSaberUI;
 #include "UnityEngine/GameObject.hpp"
 #include "UnityEngine/Resources.hpp"
 #include "UnityEngine/Object.hpp"
+#include "UnityEngine/RectOffset.hpp"
+#include "UnityEngine/RectTransform_Axis.hpp"
 using namespace UnityEngine;
+
+#include "UnityEngine/Events/UnityAction.hpp"
+using namespace UnityEngine::Events;
 
 #include "UnityEngine/UI/HorizontalLayoutGroup.hpp"
 #include "UnityEngine/UI/LayoutElement.hpp"
+#include "UnityEngine/UI/Image.hpp"
 using namespace UnityEngine::UI;
 
 #include "GlobalNamespace/LevelBar.hpp"
@@ -35,35 +41,52 @@ using namespace System::Collections;
 #include <string>
 using namespace std;
 
+#define MakeDelegate(DelegateType, varName) (il2cpp_utils::MakeDelegate<DelegateType>(classof(DelegateType), varName))
+
 DEFINE_TYPE(Slogans::UI::ViewControllers, SloganSelectionViewController);
 
 void SloganSelectionViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
     if (!firstActivation) return;
     GameObject *container = CreateScrollableSettingsContainer(this->get_transform());
-    StartCoroutine(reinterpret_cast<IEnumerator *>(CoroutineHelper::New(LoadSlogans(container->get_transform()))));
+    StartCoroutine(CoroutineHelper::New(LoadSlogans(container->get_transform())));
 }
 
 void SloganSelectionViewController::DidDeactivate(bool removedFromHierarchy, bool screenSystemDisabling) {
 
 }
 
+/*
+ * TODO: Anchors for child alignment
+ */
 custom_types::Helpers::Coroutine SloganSelectionViewController::LoadSlogans(Transform *parent) {
     std::vector<SloganConfig *> configs = SloganConfig::LoadAllConfigs();
 
     for (SloganConfig *config : configs) {
         auto layout = CreateHorizontalLayoutGroup(parent);
-        layout->set_spacing(3.0f);
-        auto layoutTransform = config->info.layout->get_transform();
-        layout->GetComponent<Backgroundable*>()->ApplyBackgroundWithAlpha(il2cpp_utils::createcsstr("round-rect-panel"), 0.5f);
+        layout->set_padding(RectOffset::New_ctor(2, 2, 0, 0));
+        layout->set_childForceExpandWidth(false);
+        layout->GetComponent<LayoutElement *>()->set_minWidth(85.0f);
+        auto layoutTransform = layout->get_transform();
+        layout->GetComponent<Backgroundable *>()->ApplyBackgroundWithAlpha("round-rect-panel", 0.5f);
 
         auto textLayout = CreateVerticalLayoutGroup(layoutTransform);
+        textLayout->set_childAlignment(TextAnchor::MiddleLeft);
+        textLayout->set_spacing(0.2f);
 
         auto nameText = CreateText(textLayout->get_transform(), config->text);
+        nameText->set_fontSize(5.0f);
         auto color = config->color;
         nameText->set_color(Color(color.r, color.g, color.b, color.a));
 
         auto idText = CreateText(textLayout->get_transform(), config->id);
+        idText->set_fontSize(3.0f);
+
+        auto selectButton = CreateUIButton(layout->get_transform(), "Select",
+        [config]() {
+
+        });
 
         co_yield nullptr;
     }
+    co_return;
 }
